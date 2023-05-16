41e10aa3c62d29fea793dce672c630cf2d4171c1
BibiaiAgency <pedro@bibiai.es>
2023-05-16 21:02:59 +0200

    Plantilla de contenidos Citas

---
+++ src/Controller/CitasAdminController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\segura_viudas_citas\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\NodeInterface;
+
+class CitasAdminController extends ControllerBase {
+
+  public function viewCitas(NodeInterface $node) {
+    if ($node->getType() != 'citas') {
+      throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException();
+    }
+
+    $build = [];
+    $build['#theme'] = 'node';
+    $build['#node'] = $node;
+    $build['#view_mode'] = 'full';
+    $build['#title'] = $node->getTitle();
+
+    return $build;
+  }
+}
