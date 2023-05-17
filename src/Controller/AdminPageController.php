fd65e976d2f1ab3a2ecc4ec26b2b758242247b26
BibiaiAgency <pedro@bibiai.es>
2023-05-17 02:03:44 +0200

    Página de administración principal modificada

--- src/Controller/AdminPageController.php
+++ src/Controller/AdminPageController.php
@@ -9,12 +9,36 @@ class AdminPageController extends ControllerBase {
     public function content() {
         $build = [
           '#type' => 'markup',
-          '#markup' => $this->t('Hola, esta es tu página personalizada.'),
-          '#attached' => [
-            'library' => [
-              'segura_viudas_citas/custom_admin_page_styles',
-            ],
-          ],
+          '#markup' => $this->t('
+          <div class="layout-row clearfix">
+          <div class="layout-column layout-column--half">
+              <div class="panel gin-layer-wrapper">
+                  <div class="panel__content">
+                      <dl class="admin-list--panel admin-list gin-layer-wrapper">
+                          <div class="admin-item">
+                            <a href="#" class="admin-item__link"></a>
+                            <dt class="admin-item__title">Franjas horarias</dt>
+                            <dd class="admin-item__description">Selecciona los horarios de disponibilidad en lo que los usuarios podrán reservar.</dt>
+                          </div>
+                      </dl>
+                  </div>
+              </div>
+              </div>
+              <div class="layout-column layout-column--half">
+              <div class="panel gin-layer-wrapper">
+                  <div class="panel__content">
+                      <dl class="admin-list--panel admin-list gin-layer-wrapper">
+                          <div class="admin-item">
+                            <a href="/admin/gestion-citas/reservas" class="admin-item__link"></a>
+                            <dt class="admin-item__title">Reservas</dt>
+                            <dd class="admin-item__description">Revisa las reservas de citas y los detalles de las citas.</dt>
+                          </div>
+                      </dl>
+                  </div>
+              </div>
+          </div>
+          
+          '),
         ];
 
         return $build;
