8c06a5813e518c64b2d1091f2fbba9ffe08ec19d
jirohn <97062301+jirohn@users.noreply.github.com>
2023-05-22 08:58:20 +0200

    añadido popup de solicitar cita a falta de estilos

--- js/segura_viudas_citas.js
+++ js/segura_viudas_citas.js
@@ -3,100 +3,55 @@
     attach: function (context, settings) {
       console.log('seguraViudasCitas behavior attached');
 
-      //si lees esto es que te sobra tiempo
 
-      /*const realFile = document.getElementById("real-file");
-      const customButton = document.getElementById("custom-button");
-      const fileName = document.getElementById("file-name");
+      var $file = $('input[type="file"]').eq(0);
+      var $file2 = $('input[type="file"]').eq(1);
 
 
-
-      function openPopUp() {
-        popUp.style.display = 'flex';
-        body.classList.add('no-scroll');
-      }
-
-      function closePopUp() {
-        popUp.style.display = 'none';
-        body.classList.remove('no-scroll');
-      }
-
-      btn.onclick = openPopUp;
-      closeBtn.onclick = closePopUp;
-
-      window.onclick = function (event) {
-        if (event.target === popUp) {
-          closePopUp();
-        }
-      }
-*/
-
-      //cosas pedralas Pedralo, estas to pedralao, el que lo desenpedre buen desenpedrelador sera//
-      // Custom File Input Button.
-      /*const realFileBtn = $(context).find('.js-form-item-field-file');
-      const customBtn = $(context).find('.custom-upload-button');
-      const customTxt = $(context).find('.custom-text');
-
-      customBtn.click(function () {
-        realFileBtn.click();
-      });
-
-      realFileBtn.change(function () {
-        if (realFileBtn.val()) {
-          customTxt.html(realFileBtn.val().match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1]);
-        } else {
-          customTxt.html('No se ha seleccionado ningún archivo...');
-        }
-      });*/
-
-      // le decimos que si presionas el boton openPopUp hace una funcion
-
-
-      // guardamos una variable con la id file-name y la añadimos en despues de los input tipo file
-
-
-
-      // guarda el primero y el segundo input del tipo file de 5 que hay en la pagina y los almacena en una variable
-      const $file = $(context).find('input[type="file"]').eq(0);
-      const $file2 = $(context).find('input[type="file"]').eq(1);
-
-
-      $file.add($file2).change(function() {
-        console.log('se cargo una foto para el popup');
+      $file.add($file2).change(function () {
+        console.log('se cargó una foto para el popup');
         if ($file.val() && $file2.val()) {
-          // le quitamos el disabled al boton
+          // Habilitar el botón
           $('.open-popup').prop('disabled', false);
-          console.log('abierto el popup');
+          console.log('se habilito el open popup');
         }
       });
 
-      const $form = $(context).find('form');
+      // Si se hace clic en el botón "open-popup", abrir el popup
+      $('.open-popup').off('click').click(function (event) {
+        console.log('se intento abrir el popup');
+
+        event.preventDefault(); // Prevenir la acción predeterminada del enlace
+        // mostramos el overlay que tiene un hide
+        $('.overlay').show();
+      });
+      var $form = $(context).find('form');
       if ($form.hasClass('segura-viudas-citas-attached')) {
         return;
       }
       $form.addClass('segura-viudas-citas-attached');
 
-      const $timeField = $form.find('select[name="field_time"]');
-      const $dateField = $form.find('input[name="field_date"]');
+      var $timeField = $form.find('select[name="field_time"]');
+      var $dateField = $form.find('input[name="field_date"]');
 
-
-      // deshabilitamos los sabados y domingos
+      // Deshabilitar los sábados y domingos
       $dateField.on('change', function () {
-        const dateValue = new Date(this.value);
-        const day = dateValue.getDay();
+        var dateValue = new Date(this.value);
+        var day = dateValue.getDay();
 
         if (day === 6 || day === 0) {
-          alert('Sábados y domingos no están disponibles para citas.');
+          alert('Los sábados y domingos no están disponibles para citas.');
           this.value = '';
         }
       });
+
       function disableTimeField() {
-        console.log('Disabling time field'); // Added console log
+        console.log('Desactivando el campo de hora');
         $timeField.prop('disabled', true);
       }
 
       function enableTimeField() {
-        console.log('Enabling time field'); // Added console log
+        console.log('Activando el campo de hora');
         $timeField.prop('disabled', false);
       }
 
@@ -109,81 +64,76 @@
       }
 
       function handleDateChange() {
-        console.log('Date field changed'); // Added console log
-        const dateValue = $dateField.val();
+        console.log('El campo de fecha ha cambiado');
+        var dateValue = $dateField.val();
         if (!dateValue) {
           disableTimeField();
         } else {
-          console.log('Fetching existing appointments for date:', dateValue); // Added console log
-          checkExistingAppointments(dateValue).done(function (response) {
-            console.log('Received appointments:', response); // Added console log
-            enableTimeField();
-            $timeField.find('option').prop('disabled', false);
-            response.forEach(function (time) {
-              $timeField.find('option[value="' + time + '"]').prop('disabled', true);
+          console.log('Obteniendo citas existentes para la fecha:', dateValue);
+          checkExistingAppointments(dateValue)
+            .done(function (response) {
+              console.log('Citas recibidas:', response);
+              enableTimeField();
+              $timeField.find('option').prop('disabled', false);
+              response.forEach(function (time) {
+                $timeField.find('option[value="' + time + '"]').prop('disabled', true);
+              });
+              $timeField.val($timeField.find('option:not(:disabled):first').val());
+            })
+            .fail(function (jqXHR, textStatus, errorThrown) {
+              console.error('Error al obtener citas:', textStatus, errorThrown);
             });
-            $timeField.val($timeField.find('option:not(:disabled):first').val());
-          }).fail(function (jqXHR, textStatus, errorThrown) {
-            console.error('Error fetching appointments:', textStatus, errorThrown); // Added console log
-          });
         }
       }
-      //llamamos al 'handleDateChange' cuando carga la pagina con la fecha ya seleccionada
+
       handleDateChange();
       $dateField.on('change', handleDateChange);
-
 
       if (!$dateField.val()) {
         disableTimeField();
       }
     },
   };
-  $(document).ready(function() {
 
+  $(document).ready(function () {
     console.log('seguraViudasCitas form styles attached');
-    // Seleccionamos los contenedores de los inputs y les añadimos las clases
+
+    // Añade clases y estilosa los contenedores de los inputs:
     var $fileDiv = $('.js-form-item-field-file').addClass('block column half right space-between').prepend('<p class="file-title">CARTA ACREDITATIVA RAÏM DO CAVA O DO CAT, O AMBDÓS*</p>');
     var $fileDiv2 = $('.js-form-item-field-file2').addClass('block column half right space-between').prepend('<p class="file-title">QUADERN DE CAMP*</p>');
     var $fileDiv3 = $('.js-form-item-field-file3').addClass('block column half right space-between').prepend('<p class="file-title">CERTIFICAT CCPAE</p>');
     var $fileDiv4 = $('.js-form-item-field-file4').addClass('block column half right space-between').prepend('<p class="file-title">RVC</p>');
     var $fileDiv5 = $('.js-form-item-field-file5').addClass('block column half right space-between').prepend('<p class="file-title">ALTRES DOCUMENTS</p>');
 
-
-
-
-
-
-    // mete los divs js-form-item-field-file y js-form-item-field-file2 dentro de un div con las clases 'block row full cgap-1 uploads nomobile'
+    // Combina los divs 'js-form-item-field-file' y 'js-form-item-field-file2' en un solo div con la clase 'block row full cgap-1 uploads nomobile'
     var $fileDivs = $fileDiv.add($fileDiv2).wrapAll('<div class="block row full cgap-1 uploads nomobile"></div>');
     var $fileDivs2 = $fileDiv3.add($fileDiv4).wrapAll('<div class="block row full cgap-1 uploads nomobile"></div>');
 
-
-
-
-    // Seleccionamos los input de tipo file y les añadimos la clase y la id correspondientes
+    // Agrega la clase 'inputfile' y un ID a los inputs de tipo file
     var $fileInput = $fileDiv.find('input[type="file"]').addClass('inputfile').attr('id', 'archivo');
     var $fileInput2 = $fileDiv2.find('input[type="file"]').addClass('inputfile').attr('id', 'archivo2');
     var $fileInput3 = $fileDiv3.find('input[type="file"]').addClass('inputfile').attr('id', 'archivo3');
     var $fileInput4 = $fileDiv4.find('input[type="file"]').addClass('inputfile').attr('id', 'archivo4');
     var $fileInput5 = $fileDiv5.find('input[type="file"]').addClass('inputfile').attr('id', 'archivo5');
 
-    // Seleccionamos los labels de los inputs que contengan la clase inputfile y les añadimos el atributo for y la clase correspondientes
+    // Agrega el atributo 'for' y la clase 'label-file' a las etiquetas de los inputs de tipo file
     var $fileLabel = $fileDiv.find('label').attr('for', 'archivo').addClass('label-file');
     var $fileLabel2 = $fileDiv2.find('label').attr('for', 'archivo2').addClass('label-file');
     var $fileLabel3 = $fileDiv3.find('label').attr('for', 'archivo3').addClass('label-file');
     var $fileLabel4 = $fileDiv4.find('label').attr('for', 'archivo4').addClass('label-file');
     var $fileLabel5 = $fileDiv5.find('label').attr('for', 'archivo5').addClass('label-file');
 
-    // Generamos el SVG y el texto de los labels
+    // Genera el SVG y el texto para las etiquetas
     var svgHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-paperclip" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#718457" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 7l-6.5 6.5a1.5 1.5 0 0 0 3 3l6.5 -6.5a3 3 0 0 0 -6 -6l-6.5 6.5a4.5 4.5 0 0 0 9 9l6.5 -6.5" /></svg>' + Drupal.t('Adjunta o arrastra el archivo');
 
-
+    // Inserta el SVG y el texto en las etiquetas de los inputs de tipo file
     $fileLabel.html(svgHTML);
     $fileLabel2.html(svgHTML);
     $fileLabel3.html(svgHTML);
     $fileLabel4.html(svgHTML);
     $fileLabel5.html(svgHTML);
 
+    // Inserta las etiquetas después de los inputs de tipo file
     $fileLabel.insertAfter($fileInput);
     $fileLabel2.insertAfter($fileInput2);
     $fileLabel3.insertAfter($fileInput3);
@@ -191,8 +141,8 @@
     $fileLabel5.insertAfter($fileInput5);
     console.log('seguraViudasCitas form styles end');
     // Crea el div contenedor con la clase 'popup'
-    var $popupDiv = $('#popup');
-
+    var $popupDiv = $('.popup');
+    var $overlay = $('.overlay');
     // Selecciona los campos del formulario y el botón de envío y los mueve al div popup
     var $fieldDate = $('#edit-field-date').detach();
     var $fieldTime = $('#edit-field-time').detach();
@@ -204,17 +154,16 @@
     var $labelTime = $('label[for="edit-field-time"]').detach();
     var $labelComment = $('label[for="edit-field-comment"]').detach();
     var $labelModalidad = $('label[for="edit-field-modalidad"]').detach();
-
     // Añade los elementos al div
     $popupDiv.append( $labelDate, $fieldDate, $labelTime, $fieldTime, $labelComment, $fieldComment, $labelModalidad, $fieldModalidad, $submit);
     console.log('añadido el popup');
     // Agrega el div popup al final del formulario
-    $('form').append($popupDiv);
-    console.log('seguraViudasCitas behavior attached');
-
+    $('form').append($overlay);
     console.log('desactivado el popup');
-    // desactivamos el open-poppup
+    // escondemos overlay para luego llamarlo con un fadein
+    $('.overlay').hide();
     $('.open-popup').prop('disabled', true);
+
   });
 
 })(jQuery, Drupal);
